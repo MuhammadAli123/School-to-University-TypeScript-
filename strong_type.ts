bd92a8841642c310d52c97e6ea3d0c50f547e3c3
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-20 21:52:42 +0500

    Add files via upload

---
+++ strong_type.ts
@@ -0,0 +1,19 @@
+
+// strong typed syntax
+
+/*let country : string = "germany";
+ country = 7;
+ console.log(country);
+/*
+var num : number = 2;
+num = "gjhgjh";
+var bool : boolean = true;
+bool  = "fgdf";
+bool = 5;
+*/
+// type inference refers to automatin detection of datatype
+/*
+var a = "name";
+var no = 50;
+var boo = false;
+*/
