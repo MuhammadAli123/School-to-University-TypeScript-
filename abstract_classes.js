c801631cc9da69554249ebf6f7365b1df06a596b
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-20 22:38:00 +0500

    Add files via upload

---
+++ abstract_classes.js
@@ -0,0 +1,28 @@
+/*
+
+abstract class world{
+     human(): string { return this.name(); }
+     abstract name() : string;
+}
+
+class data extends world{
+    name() {return "jhon"}
+}
+
+
+///var record = new world();// error, can not create instance of Abstract class
+
+var record = new data();
+console.log(record.name());*/
+/*
+function myCallBack(callbackName: string) {
+    console.log("My name is " + callbackName);
+}
+
+function callingFunction(initialText: string, callback: (text: string) => void)
+{
+    callback(initialText);
+}
+
+callingFunction("myCallBack", myCallBack);*/ 
+//# sourceMappingURL=abstract_classes.js.map
