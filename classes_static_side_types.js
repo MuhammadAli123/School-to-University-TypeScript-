2b1096ad0b8f1106741cfbe04d1c3f35b6b71334
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 15:51:25 +0500

    Update classes_static_side_types.js

--- classes_static_side_types.js
+++ classes_static_side_types.js
@@ -3,20 +3,20 @@ When working with classes and interfaces, it helps to keep in mind that a class 
 the type of the static side and the type of the instance side
 */
 //Lets us create an interface that defines a constructor
-/*
+
 interface ClockInterface {
     new (hour: number, minute: number);// this defines a constructor signature
 }
 class Clock1 implements ClockInterface  {
      currentTime : Date;
      constructor(h: number, m: number) { }
-} */
+} 
 /* Note: the constructor sits in the static side, it cannot be implemented
 if you create an interface with a construct signature and try to
 create a class that implements this interface you get an error*/
 // you would need to work with the 'static' side of the class directly. 
 /*In this example, we work with the class directly*/
-/*
+
 interface ClockStatic {
     new (hour: number, minute: number);
 }
@@ -32,5 +32,4 @@ class Clock implements MyClockInterface {
 var obj: ClockStatic = Clock;
 var a =new obj(7, 30);
 console.log(a);
-*/ 
-//# sourceMappingURL=classes_static_side_types.js.map
+
