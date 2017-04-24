d024a37fd9d0878edaf4e26582b31157aacc1261
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 15:39:47 +0500

    Update interfaces_funtionalTypes.ts

--- interfaces_funtionalTypes.ts
+++ interfaces_funtionalTypes.ts
@@ -5,7 +5,7 @@ Java or C# where we can only use interfaces to describe class types.
 /*here we are going to defining a interface with a single function, any function with the 
 same signature will have this type
 */ 
-/*
+
 interface Human{
     (Name: string , Age: string):boolean
 }
@@ -20,7 +20,7 @@ var world:Human = function(name: string, age: string){
                                 return true;
                               }
 };
-*/
+
 //For function types to correctly type-check, the name of the parameters do not need to match. 
 //Function parameters are checked one at a time, with the type in each corresponding parameter 
 //position checked against each other.  
