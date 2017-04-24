b542cd42b39c8067486202680d2d2aa3279d8ad4
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 14:36:24 +0500

    Update union_types.js

--- union_types.js
+++ union_types.js
@@ -1,4 +1,4 @@
-/*
+
 function Info(firstName: string|number|boolean, age: string|number|boolean):string|number|boolean{
     if(typeof age === "string"){ // this is know as type guard or union types
         // age is treated as a string here
@@ -12,9 +12,9 @@ function Info(firstName: string|number|boolean, age: string|number|boolean):stri
         // age is treated as a boolean here
         return age +" is a boolean";
     }
-}*/
+}
 // other example
-/*
+
 class human{food(){}}
 class animal{eat(){}}
 
@@ -24,7 +24,7 @@ if(world instanceof human){// this is type guard
 }
 else{
     world.eat();
-}*/
+}
 /*Note on Type Guards:
 A common pattern in JavaScript is to use typeof or instanceof to examine the type of an expression at runtime.
 TypeScript now understands these conditions and will change type inference accordingly when used in an if block.
