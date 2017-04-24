e37b2b2cd9c0a7b47e67fe31f62cedd29110e2ce
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 14:14:15 +0500

    Update function_default_parameter.js

--- function_default_parameter.js
+++ function_default_parameter.js
@@ -1,5 +1,5 @@
 // Named function with optional and default parameters
-/*function Info(firstName: string, lastName = "suns"): string{
+function Info(firstName: string, lastName = "suns"): string{
   if(lastName){
       return firstName+" "+lastName;
     }
@@ -11,5 +11,3 @@ var result1 = Info("jhon"); //works correctly because last parameter is default
 console.log(result1);
 var result2 = Info("pretty","roses"); //correct because last parameter in optional also
 console.log(result2);
-*/ 
-//# sourceMappingURL=function_default_parameter.js.map
