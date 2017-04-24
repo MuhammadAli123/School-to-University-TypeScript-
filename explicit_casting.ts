dbbe318596454c70c5a200fe863e2e9cd7c25fe0
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 14:09:38 +0500

    Update explicit_casting.ts

--- explicit_casting.ts
+++ explicit_casting.ts
@@ -3,19 +3,19 @@
 explicit casting */
 /* A object can be cast by using <> sybtax */ 
 /* It allows us to explicitly cast from any type to a datatype "any"*/
-/*
+
 let ExType =  {name: "name", id: 1} as any;
 ExType = {name: "newName", id: 2};  //assign a type which has the the same properties
 ExType = {name: "newName", id: 2, age: 3};//can add a property
 ExType = { id: 2, age: 4};//can even reduce the properties because of initial any explict casting
-*/
 
 
 
 
 
 
-/*
+
+
 
 // Named function  
   function num(z: number, y: number): number {//Named function
@@ -33,4 +33,3 @@ console.log(num(2,4));
 console.log(num2(3,4));
 console.log(num3(4,4,));
 
-*/
