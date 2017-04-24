e70d07b80a5da8a9131f91a18459bbd0795dd5ed
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 14:22:32 +0500

    Update function_lambda.ts

--- function_lambda.ts
+++ function_lambda.ts
@@ -1,6 +1,6 @@
 // These functions know as fat arrow(because -> is a thin arrow and => is a fat arrow) 
 // functions and also called lambda functions
-/*
+
 var Info = (firstName:string, lastName:string) =>{
     return firstName + " " + lastName;
 }
@@ -8,7 +8,7 @@ var record = Info("blue", "sky");
 console.log(record);
 
 var Info = (firstName:string, lastName:string) => (firstName + "" + lastName);
-*/
+
 
 //function add(arg1: string, arg2: string): string{return}
 //function add2(arg1: number, arg2: number): number{return};
