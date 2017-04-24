f5bcffbbdc8eaaaf88a29092235c0fe2bd15af47
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 15:58:28 +0500

    Update interfaces_hybrid_types.js

--- interfaces_hybrid_types.js
+++ interfaces_hybrid_types.js
@@ -2,7 +2,7 @@
 hybrid type is an object that acts as both a function and an object, with additional
 properties
 */
-/*
+
 interface world{
     (person: string): string;//Note: there is no name assigned to the function signature
     age: number;
@@ -12,8 +12,7 @@ var record: world;
 record("jack");
 record.age = 25;
 record.Data();
-*/
+
 // Note:When interacting with 3rd-party JavaScript, you may need to use patterns like the 
 // above to fully-describe the shape of the type.
 /* Note: The transpiled Javascript code will not run here it is being used as an example*/ 
-//# sourceMappingURL=interfaces_hybrid_types.js.map
