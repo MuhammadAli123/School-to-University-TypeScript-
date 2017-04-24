43858ce770a9379dba33a6d379bc5d91e96f06b8
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 13:58:44 +0500

    Update strong_type.ts

--- strong_type.ts
+++ strong_type.ts
@@ -2,7 +2,7 @@
 // strong types syntax
 
 let country: string = "china";
-//country =7;         // Error
+//country = 7;         // Error
 console.log(country);
 var num: number = 6;
 //num = "ghjgjh";    // Error
@@ -10,11 +10,11 @@ console.log(num);
 
 var bool: boolean = true;
 //bool = "vfghg";   // Error
-//bool =9;          // Error
+//bool = 9;          // Error
 console.log(bool);
 
 // type inference refers to automation detection of datatype
 
 var a = "name";
-var no =7;
+var no = 7;
 var boo = true;
