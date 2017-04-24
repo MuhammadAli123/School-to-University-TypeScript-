ee82041a3620ea3ce67e1ab2e2a7385b84036f3b
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 13:58:32 +0500

    Update strong_type.js

--- strong_type.js
+++ strong_type.js
@@ -1,18 +1,19 @@
-// strong typed syntax
-/*let country : string = "germany";
- country = 7;
- console.log(country);
-/*
-var num : number = 2;
-num = "gjhgjh";
-var bool : boolean = true;
-bool  = "fgdf";
-bool = 5;
-*/
-// type inference refers to automatin detection of datatype
-/*
+// strong types syntax
+
+let country: string = "china";
+//country = 7;
+console.log(country);
+var num: number = 6;
+//num = "ghjgjh";
+console.log(num);
+
+var bool: boolean = true;
+//bool = "vfghg";
+//bool = 9;
+console.log(bool);
+
+// type inference refers to automation detection of datatype
+
 var a = "name";
-var no = 50;
-var boo = false;
-*/ 
-//# sourceMappingURL=strong_type.js.map
+var no = 7;
+var boo = true;
