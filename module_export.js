55b6fa541ac58986f4b6afb88884bc5fc502882a
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 16:36:33 +0500

    Update module_export.js

--- module_export.js
+++ module_export.js
@@ -1,9 +1,7 @@
-/*
+
 export function add(a: number, b: number){
     return a + b;
 }
 export function multiple(c: number, d: number){
      return c * d;
 }
-*/ 
-//# sourceMappingURL=module_export.js.map
