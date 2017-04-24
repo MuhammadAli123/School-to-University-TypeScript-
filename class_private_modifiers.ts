2e91dd1f0b24d5f9be6c06c614bc2bd2bf2df185
MuhammadAli123 <thunder_storm2612@yahoo.com>
2017-04-24 15:15:04 +0500

    Update class_private_modifiers.ts

--- class_private_modifiers.ts
+++ class_private_modifiers.ts
@@ -1,5 +1,5 @@
 // In TypeScript, each member is public by default.
-/*
+
 class Human{
     private name:string;
     constructor(FullName: string) { 
@@ -11,4 +11,4 @@ class Human{
 }
 // private members can only be access to their respective class
 var record = new Human("micheal").name;
-console.log(record);*/
+console.log(record);
